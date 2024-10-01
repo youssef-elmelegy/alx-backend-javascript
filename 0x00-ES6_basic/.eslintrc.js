85fe4ebd5f49144978af44d8dff90893f5757cc8
youssef-elmelegy <youssefelmelegy999@gmail.com>
2024-09-30 17:57:06 -0700

    Rest parameter

---
+++ 0x00-ES6_basic/.eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+  ],
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  rules: {
+    'no-unused-vars': 'warn',
+    'no-console': 'off',
+    'strict': ['error', 'global'],
+  },
+};
+
